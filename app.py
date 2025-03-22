240ff5d66a3c281e575e5229e5af809e7d90144a
agonzalez9784 <agonzalez9784@gmail.com>
2025-03-22 18:58:53 -0500

    Added authentication and config.json to configure admin login. All admin endpoints should require authentication now. Resolving issue #8.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import logging
 import pandas as pd
 from pandasql import sqldf
+import json
 
 app = Flask(__name__, template_folder="templates")
 app.secret_key = "HI"
@@ -282,178 +283,223 @@ def notFound(e):
 @app.route("/admin")
 def admin():
 
-    return render_template('admin/admin.html')
+    if(session['adminAuth']):
+        return render_template('admin/admin.html')
+    
+    return redirect("/admin/login")
 
 @app.route('/admin/res/<path:filename>')
 def admin_res(filename):
-    return send_from_directory('restricted', filename)
+    if(session['adminAuth']):
+        return send_from_directory('restricted', filename)
+    
+    return 404
+
+@app.route("/admin/logout")
+def admin_logout():
+    session['adminAuth'] = False
+
+    return redirect('/admin/login')
 
 @app.route("/admin/login")
 def admin_login():
     
-    return ""
+    return render_template('admin/login.html')
 
 @app.route("/admin/actions/deleteOrder/<orderID>")
 def admin_deleteorder(orderID):
 
-    print("Request received")
-    conn = sqlite3.connect("db/pizzadb.db")
-    cursor = conn.cursor()
-    
-    cursor.execute("DELETE FROM Orders WHERE OrderID = ?;", (orderID,))
+    if(session['adminAuth']):
+        print("Request received")
+        conn = sqlite3.connect("db/pizzadb.db")
+        cursor = conn.cursor()
+        
+        cursor.execute("DELETE FROM Orders WHERE OrderID = ?;", (orderID,))
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
 
-    return "test"
+        return "test"
+
+    return redirect('/admin/login')
 
 @app.route("/admin/actions/updateOrder")
 def admin_updateorder():
-    orderID = request.args.get("orderID")
-    first_name = request.args.get("firstName")
-    last_name = request.args.get("lastName")
-    card_no = request.args.get("cardNo")
-    total_cost = request.args.get("totalCost")
-    date = request.args.get("date")
-    active = request.args.get("active")
 
-    print(date)
-    conn = sqlite3.connect("db/pizzadb.db")
-    cursor = conn.cursor()
+    if(session['adminAuth']):
+        orderID = request.args.get("orderID")
+        first_name = request.args.get("firstName")
+        last_name = request.args.get("lastName")
+        card_no = request.args.get("cardNo")
+        total_cost = request.args.get("totalCost")
+        date = request.args.get("date")
+        active = request.args.get("active")
 
-    cursor.execute("""UPDATE Orders SET FirstName = ?, LastName = ?, 
-                      CardNo = ?, TotalCost = ?, date = ?, ACTIVE = ?
-                   WHERE OrderID = ?;""", (first_name, last_name, card_no, total_cost, date, active, orderID))
-    
-    conn.commit()
-    conn.close()
+        print(date)
+        conn = sqlite3.connect("db/pizzadb.db")
+        cursor = conn.cursor()
 
-    return "200"
+        cursor.execute("""UPDATE Orders SET FirstName = ?, LastName = ?, 
+                        CardNo = ?, TotalCost = ?, date = ?, ACTIVE = ?
+                    WHERE OrderID = ?;""", (first_name, last_name, card_no, total_cost, date, active, orderID))
+        
+        conn.commit()
+        conn.close()
 
+        return "200"
 
+    return redirect('/admin/login')
 
 @app.route("/admin/action/getSalesData")
 def get_salesdata():
-
-    totalRevenue = 0
-    noSales = 0
-    noSalesPerMonth = {'Jan': 0, 'Feb': 0, 'Mar': 0, 'Apr': 0, 'May': 0, 'Jun': 0, 'Jul': 0, 'Aug': 0, 'Sep': 0, 'Oct': 0, 'Nov': 0, 'Dec': 0}
-    revenuePerMonth = {'Jan': 0, 'Feb': 0, 'Mar': 0, 'Apr': 0, 'May': 0, 'Jun': 0, 'Jul': 0, 'Aug': 0, 'Sep': 0, 'Oct': 0, 'Nov': 0, 'Dec': 0}
-
-    salesDataQuery = []
-    conn = sqlite3.connect("db/pizzadb.db")
-
-    cursor = conn.cursor()
-
-    today = datetime.today()
     
-    cursor.execute("SELECT * FROM Orders WHERE date BETWEEN '"+str(today.year)+"-01-01' AND '"+str(today.year)+"-12-31';")
-    rows = cursor.fetchall()
+    if(session['adminAuth']):
+        totalRevenue = 0
+        noSales = 0
+        noSalesPerMonth = {'Jan': 0, 'Feb': 0, 'Mar': 0, 'Apr': 0, 'May': 0, 'Jun': 0, 'Jul': 0, 'Aug': 0, 'Sep': 0, 'Oct': 0, 'Nov': 0, 'Dec': 0}
+        revenuePerMonth = {'Jan': 0, 'Feb': 0, 'Mar': 0, 'Apr': 0, 'May': 0, 'Jun': 0, 'Jul': 0, 'Aug': 0, 'Sep': 0, 'Oct': 0, 'Nov': 0, 'Dec': 0}
 
-    mos = {1:'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'}
-    # SELECT COUNT(OrderID), SUM(TotalCost), strftime('%m', Date) AS Month FROM ORDERS GROUP BY Month;
+        salesDataQuery = []
+        conn = sqlite3.connect("db/pizzadb.db")
 
-    for row in rows:
-        rowData = {'orderID': row[0], 
-                'receiptNo.': row[1], 
-                'date': row[2], 
-                'first_name': row[3], 
-                'last_name': row[4], 
-                'card_no': row[5], 
-                'total_cost':row[6],
-                'active': row[7],
-                'orderSerial': row[8], 
-                'month': mos[datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S').month]}
+        cursor = conn.cursor()
+
+        today = datetime.today()
         
-        totalRevenue += float(row[6])
-        noSales += 1
-        revenuePerMonth[rowData['month']]+=float(row[6])
-        noSalesPerMonth[rowData['month']]+=1
+        cursor.execute("SELECT * FROM Orders WHERE date BETWEEN '"+str(today.year)+"-01-01' AND '"+str(today.year)+"-12-31';")
+        rows = cursor.fetchall()
 
-        salesDataQuery.append(rowData)
+        mos = {1:'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'}
+        # SELECT COUNT(OrderID), SUM(TotalCost), strftime('%m', Date) AS Month FROM ORDERS GROUP BY Month;
 
-    conn.commit()
-    conn.close()
+        for row in rows:
+            rowData = {'orderID': row[0], 
+                    'receiptNo.': row[1], 
+                    'date': row[2], 
+                    'first_name': row[3], 
+                    'last_name': row[4], 
+                    'card_no': row[5], 
+                    'total_cost':row[6],
+                    'active': row[7],
+                    'orderSerial': row[8], 
+                    'month': mos[datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S').month]}
+            
+            totalRevenue += float(row[6])
+            noSales += 1
+            revenuePerMonth[rowData['month']]+=float(row[6])
+            noSalesPerMonth[rowData['month']]+=1
 
-    salesData = {'totalRevenue': totalRevenue, 'noSales': noSales, 'revenuePerMonth': revenuePerMonth, 'noSalesPerMonth': noSalesPerMonth}
+            salesDataQuery.append(rowData)
 
-    return salesData
+        conn.commit()
+        conn.close()
 
+        salesData = {'totalRevenue': totalRevenue, 'noSales': noSales, 'revenuePerMonth': revenuePerMonth, 'noSalesPerMonth': noSalesPerMonth}
+
+        return salesData
+    
+    return redirect("/admin/login")
+
+
+@app.route("/admin/auth")
+def authenticate():
+    username = request.args.get("username")
+    password = request.args.get("password")
+
+    admin_username = ''
+    admin_password = ''
+    
+    with open('config.json', 'r') as file:
+        data = json.load(file)
+        admin_username = data['admin_username']
+        admin_password = data['admin_password']
+
+    if(username == admin_username and password == admin_password):
+        session['adminAuth'] = True
+
+        return redirect("/admin")
+    else:
+
+        return redirect('/admin/login')
+    
 @app.route("/admin/action/getItemData")
 def get_itemdata():
-
-    itemsData = {}
-
-
-    #i don't care if it's ugly rn, i'm going to do a major refactor in the near future, so this will be fine for now.
-
-    conn = sqlite3.connect("db/pizzadb.db")
-
-    cursor = conn.cursor()
-
-    cursor.execute('SELECT ItemName, COUNT(ReceiptNo) FROM ReceiptItems GROUP BY ItemName;')
-
-    rows = cursor.fetchall()
-
-    labels = []
-    values = []
-    for row in rows:
-        labels.append(row[0])
-        values.append(row[1])
-
-    data1 = {'labels': labels, 'values': values}
     
-    conn.commit()
-    conn.close()
-    
-    conn = sqlite3.connect("db/pizzadb.db")
+    if(session['adminAuth']):
+        itemsData = {}
 
-    cursor = conn.cursor()
 
-    cursor.execute("SELECT ItemName, COUNT(ItemName), strftime('%m', Date) AS Month FROM ReceiptItems LEFT JOIN Orders ON ReceiptItems.OrderID = Orders.OrderID GROUP BY ItemName;")
-    rows = cursor.fetchall()
-    data2 = []
-    for row in rows:
-        data2.append((row[0], row[1], row[2]))
+        #i don't care if it's ugly rn, i'm going to do a major refactor in the near future, so this will be fine for now.
 
-    conn.commit()
-    conn.close()
+        conn = sqlite3.connect("db/pizzadb.db")
 
-    # SELECT ItemName, COUNT(ReceiptNo) FROM ReceiptItems GROUP BY ItemName;
-    # SELECT ItemName, COUNT(ItemName), strftime('%m', Date) AS Month FROM ReceiptItems LEFT JOIN Orders ON ReceiptItems.OrderID = Orders.OrderID GROUP BY ItemName;
+        cursor = conn.cursor()
 
-    return {'totalItemCount': data1, 'itemsCountPerMonth': data2}
+        cursor.execute('SELECT ItemName, COUNT(ReceiptNo) FROM ReceiptItems GROUP BY ItemName;')
+
+        rows = cursor.fetchall()
+
+        labels = []
+        values = []
+        for row in rows:
+            labels.append(row[0])
+            values.append(row[1])
+
+        data1 = {'labels': labels, 'values': values}
+        
+        conn.commit()
+        conn.close()
+        
+        conn = sqlite3.connect("db/pizzadb.db")
+
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT ItemName, COUNT(ItemName), strftime('%m', Date) AS Month FROM ReceiptItems LEFT JOIN Orders ON ReceiptItems.OrderID = Orders.OrderID GROUP BY ItemName;")
+        rows = cursor.fetchall()
+        data2 = []
+        for row in rows:
+            data2.append((row[0], row[1], row[2]))
+
+        conn.commit()
+        conn.close()
+
+        # SELECT ItemName, COUNT(ReceiptNo) FROM ReceiptItems GROUP BY ItemName;
+        # SELECT ItemName, COUNT(ItemName), strftime('%m', Date) AS Month FROM ReceiptItems LEFT JOIN Orders ON ReceiptItems.OrderID = Orders.OrderID GROUP BY ItemName;
+
+        return {'totalItemCount': data1, 'itemsCountPerMonth': data2}
+
+    return redirect('/admin/login')
 
 @app.route("/admin/actions/getOrders")
 def admin_getorders():
     
+    if(session['adminAuth']):
+        conn = sqlite3.connect("db/pizzadb.db")
+        cursor = conn.cursor()
 
-    conn = sqlite3.connect("db/pizzadb.db")
-    cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Orders;")
+        rows = cursor.fetchall()
 
-    cursor.execute("SELECT * FROM Orders;")
-    rows = cursor.fetchall()
+        data = []
 
-    data = []
+        for row in rows:
+            rowData = {'orderID': row[0], 
+                    'receiptNo.': row[1], 
+                    'date': row[2], 
+                    'first_name': row[3], 
+                    'last_name': row[4], 
+                    'card_no': row[5], 
+                    'total_cost':row[6],
+                    'active': row[7],
+                    'orderSerial': row[8],
+                    'status': "idk"}
+            
+            data.append(rowData)
+        conn.commit()
+        conn.close()
 
-    for row in rows:
-        rowData = {'orderID': row[0], 
-                'receiptNo.': row[1], 
-                'date': row[2], 
-                'first_name': row[3], 
-                'last_name': row[4], 
-                'card_no': row[5], 
-                'total_cost':row[6],
-                'active': row[7],
-                'orderSerial': row[8],
-                'status': "idk"}
-        
-        data.append(rowData)
-    conn.commit()
-    conn.close()
-
-    return data
-
+        return data
+    return redirect('/admin/login')
 
 
 #delete later
